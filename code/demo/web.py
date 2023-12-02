709de2a16873efb81fbb03e3e40dc5c788946243
zzz <xaozhuge@163.com>
2023-12-02 19:23:17 +0800

    python的多路由

--- code/demo/web.py
+++ code/demo/web.py
@@ -6,5 +6,20 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
+@app.route('/route1')
+def route1():
+    return 'This is route 1'
+
+# 第二个路由
+@app.route('/route2')
+def route2():
+    return 'This is route 2'
+
+# 通用路由处理函数
+# @app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return f'This is a catch-all route. Path: {path}'
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
