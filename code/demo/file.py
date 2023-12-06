8b4b7a1384edaccee157acc2396762e21995798e
zzz <xaozhuge@163.com>
2023-12-06 10:16:04 +0800

    file.py

---
+++ code/demo/file.py
@@ -0,0 +1,26 @@
+
+file_path = 'demo/remark'
+
+# 读取文件的所有内容
+try:
+    with open(file_path, 'r') as file:
+        file_contents = file.read()
+        print(file_contents)
+except FileNotFoundError:
+    print(f"File not found: {file_path}")
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+
+# 按行读取
+try:
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+        for line in lines:
+            print(line.strip())  # strip() 方法用于去除行尾的换行符
+
+except FileNotFoundError:
+    print(f"File not found: {file_path}")
+except Exception as e:
+    print(f"An error occurred: {e}")
