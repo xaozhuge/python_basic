0e6c8f53a9c2163feee2eafa01218b9acc14f15b
zzz <xaozhuge@163.com>
2023-12-12 09:29:08 +0800

    loop_while.py

---
+++ code/demo/loop_while.py
@@ -0,0 +1,16 @@
+from common.common import loga
+
+# 示例：使用 while 循环计数
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+
+
+'''
+docker exec python380_c python3 demo/loop_while.py
+'''
+
+'''
+1. while循环
+'''
