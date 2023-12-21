ef28d0f3691788c423885876b1994e4330289f8b
zzz <xaozhuge@163.com>
2023-12-21 09:14:01 +0800

    judgetype.py

--- code/demo/judgetype.py
+++ code/demo/judgetype.py
@@ -1,21 +1,74 @@
-# 定义不同类型的变量
-integer_variable = 42
-float_variable = 3.14
-string_variable = "Hello, World!"
-list_variable = [1, 2, 3]
-tuple_variable = (1, 2, 3)
-dict_variable = {"key": "value"}
-boolean_variable = True
 
-# 使用 type() 函数判断变量类型
-print(type(integer_variable))  # <class 'int'>
-print(type(float_variable))    # <class 'float'>
-print(type(string_variable))   # <class 'str'>
-print(type(list_variable))     # <class 'list'>
-print(type(tuple_variable))    # <class 'tuple'>
-print(type(dict_variable))     # <class 'dict'>
-print(type(boolean_variable))  # <class 'bool'>
+def typeInt():
+	print("typeInt()-------------")
+	var = 42
+	var_type = type(var)
+	print(f"值为: {var} ，类型为: {var_type}")
+	print()
+
+def typeFloat():
+	print("typeFloat()-------------")
+	var = 3.14
+	var_type = type(var)
+	print(f"值为: {var} ，类型为: {var_type}")
+	print()
+
+def typeStr():
+	print("typeStr()-------------")
+	var = "Hello, World!"
+	var_type = type(var)
+	print(f"值为: {var} ，类型为: {var_type}")
+	print()
+
+def typeList():
+	print("typeList()-------------")
+	var = [1, 2, 3]
+	var_type = type(var)
+	print(f"值为: {var} ，类型为: {var_type}")
+	print()
+
+def typeTuple():
+	print("typeTuple()-------------")
+	var = (1, 2, 3)
+	var_type = type(var)
+	print(f"值为: {var} ，类型为: {var_type}")
+	print()
+
+def typeDict():
+	print("typeDict()-------------")
+	var = {"key": "value"}
+	var_type = type(var)
+	print(f"值为: {var} ，类型为: {var_type}")
+	print()
+
+def typeBool():
+	print("typeBool()-------------")
+	var = True
+	var_type = type(var)
+	print(f"值为: {var} ，类型为: {var_type}")
+	print()	
+
+def typeRange():
+	print("typeRange()-------------")
+	var = range(1, 6)
+	var_type = type(var)
+	print(f"值为: {var} ，类型为: {var_type}")
+	print()	
+
+typeInt()
+typeFloat()
+typeStr()
+typeList()
+typeTuple()
+typeDict()
+typeBool()
+typeRange()
+
 
 '''
 docker exec python380_c python3 demo/judgetype.py
+'''
+
+'''
+1. 使用 type() 函数判断变量类型
 '''
