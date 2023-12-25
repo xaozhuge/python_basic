48beb01fd53de677d06bcbcf4fb80c811ccbfab5
zzz <xaozhuge@163.com>
2023-12-25 09:18:52 +0800

    type_string.py and type_type.py

--- code/demo/type_string.py
+++ code/demo/type_string.py
@@ -1,81 +1,69 @@
 
 
+from common.common import d
+from common.common import pe
+
 def selectStringElement():
-	print('selectStringElement()--------------')
+	pe("selectStringElement")
 	my_str = "Hello, World!"
-	print(my_str)
-	print("----------------")
+	d(my_str)
 	# 返回第一次出现的位置，如果没有找到返回-1
 	index1 = my_str.find("World")
-	print(index1)
-	print("----------------")
+	d(index1)
 	# 返回第一次出现的位置，如果没有找到抛出ValueError
 	index2 = my_str.index("World")  
-	print(index2)
-	print()
+	d(index2)
 
 
 def updateStringElement():
-	print('updateStringElement()--------------')
+	pe("updateStringElement")
 	original_str = "Hello, World!"
-	print(original_str)
-	print("----------------")
+	d(original_str)
 	new_str = original_str.replace("Hello", "Hi")
-	print(new_str)  # 输出: Hi, World!
-	print()
+	d(new_str)  # 输出: Hi, World!
 
 
 def deleteStringElement():
-	print('deleteStringElement()--------------')
+	pe("deleteStringElement")
 	original_str = "Hello, World!"
-	print(original_str)
-	print("----------------")
+	d(original_str)
 	new_str = original_str[:5] + original_str[7:]
-	print(new_str)  # 输出: Hello World!
-	print()
+	d(new_str)  # 输出: Hello World!
 
 def addStringElement():
-	print('addStringElement()--------------')
+	pe("addStringElement")
 	str1 = "Hello"
-	print(str1)
+	d(str1)
 	str2 = "World"
-	print(str2)
-	print("----------------")
+	d(str2)
 	result = str1 + " " + str2
-	print(result)  # 输出: Hello World
-	print("----------------")
+	d(result)  # 输出: Hello World
 
 	#使用 format格式化字符串
 	name = "Alice"
 	age = 30
-	print(name)
-	print(age)
-	print("----------------")
+	d(name)
+	d(age)
 	result = "My name is {} and I am {} years old.".format(name, age)
 	# 输出: My name is Alice and I am 30 years old.
-	print(result)
-	print("----------------")
+	d(result)
 
 	#使用f-strings
 	name = "Bob"
 	age = 25
-	print(name)
-	print(age)
-	print("----------------")
+	d(name)
+	d(age)
 	result = f"My name is {name} and I am {age} years old."
-	print(result)
+	d(result)
 	# 输出: My name is Bob and I am 25 years old.
-	print()
 
 
 def selectString():
-	print('selectString()--------------')
+	pe("selectString")
 	my_str = "abcaabcaabca"
-	print(my_str)
-	print("----------------")
+	d(my_str)
 	count = my_str.count("abc")
-	print(count)  # 输出: 3
-	print()
+	d(count)  # 输出: 3
 
 
 selectStringElement()
