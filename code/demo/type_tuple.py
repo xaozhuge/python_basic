13d2a3cdffc3d3962a1d4f00dfcd8dad3b1b3773
zzz <xaozhuge@163.com>
2023-12-25 09:15:19 +0800

    type_tuple.py

--- code/demo/type_tuple.py
+++ code/demo/type_tuple.py
@@ -1,65 +1,63 @@
 
+
+from common.common import d
+from common.common import pe
+
 def createTuple():
-	print('createTuple()--------------')
+	pe("createTuple")
 	# 定义元组
 	my_tuple = (1, 2, 3, 'a', 'b')
-	print(my_tuple)
-	print('--------------')
+	d(my_tuple)
 	# 元组切片
 	sub_tuple = my_tuple[1:4]
-	print(sub_tuple)
-	print('--------------')
+	d(sub_tuple)
 	# 创建新的元组
 	my_tuple = my_tuple + (4, 5, 6)
-	print(my_tuple)
-	print()
+	d(my_tuple)
 	
 def selectTupleElement():
-	print('selectTupleElement()--------------')
+	pe("selectTupleElement")
 	my_tuple = (1, 2, 3, 'a', 'b')
-	print(my_tuple)
-	print('--------------')
+	d(my_tuple)
 
 	# 获取元素
 	element = my_tuple[0]
-	print(element)
-	print()
+	d(element)
 
 # 由于元组的不可变性，无法对元组进行直接的增删改操作。
 # 如果需要修改元组，可以先将元组转换为列表进行操作，然后再转回元组。
 def updateTupleElement():
-	print('updateTupleElement()--------------')
+	pe("updateTupleElement")
 	my_tuple = (1, 2, 3, 'a', 'b')
-	print(my_tuple)
-	print('--------------')
+	d(my_tuple)
 
 	# 将元组转换为列表
 	my_list = list(my_tuple)
+	d(my_list)
+	
 	# 在列表中进行增删改操作
 	my_list[1] = 'x'
-	print(my_list)
-	print('--------------')
+	d(my_list)
+
 	my_list.append(4)
-	print(my_list)
-	print('--------------')
+	d(my_list)
+
 	my_list.remove(3)
 	# 将列表转换回元组
 	modified_tuple = tuple(my_list)
-	print(modified_tuple)
-	print()
+	d(modified_tuple)
 
 def deleteTuple():
-	print('deleteTuple()--------------')
+	pe("deleteTuple")
 	my_tuple = (1, 2, 3, 'a', 'b')
-	print(my_tuple)
-	print('--------------')
+	d(my_tuple)
 	
 	exist = 'my_tuple' in dir()
-	print(exist)
-	print('--------------')
+	d(exist)
+
 	del my_tuple
 	exist = 'my_tuple' in dir()
-	print(exist)
+	d(exist)
 
 
 createTuple()
