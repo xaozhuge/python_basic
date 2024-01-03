d27fec06ca56f0ba038fd240781bcf54deed5d12
zzz <xaozhuge@163.com>
2024-01-03 09:22:30 +0800

    async_use_01.py

---
+++ code/demo/async_use_01.py
@@ -0,0 +1,60 @@
+import asyncio
+import time
+from common.common import d
+from common.common import pe
+
+import time
+import asyncio
+
+
+async def washing1():
+    time.sleep(1)  # 第一台洗衣机,
+    print('washer1 finished')  # 洗完了
+
+
+async def washing2():
+    time.sleep(5)
+    print('washer2 finished')
+
+
+async def washing3():
+    time.sleep(3)
+    print('washer3 finished')
+
+
+async def multiTask():
+    print('start main:')
+    start_time = time.time()
+    task1 = asyncio.create_task(washing1())
+    task2 = asyncio.create_task(washing2())
+    task3 = asyncio.create_task(washing3())
+    await task1
+    await task2
+    await task3
+    end_time = time.time()
+    print('-----------end main----------')
+    print('总共耗时:{}'.format(end_time-start_time))
+
+
+# 第一种方式
+# asyncio.run(multiTask())
+
+# 第二种方式
+# 创建一个事件循环
+loop = asyncio.get_event_loop()
+# 将协程对象加入到事件循环中，并执行
+result = loop.run_until_complete(multiTask())  
+
+
+'''
+docker exec python380_c python3 demo/async_use_01.py
+'''
+
+'''
+1. 多任务的异步的处理，通过 create_task + await 方式
+
+
+
+
+
+'''
