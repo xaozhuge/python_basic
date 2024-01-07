4d7cec335beb757c0a3d94047701aa28350f8d0e
zzz <xaozhuge@163.com>
2024-01-07 18:24:24 +0800

    hello.py

---
+++ code/python100days/day01/hello.py
@@ -0,0 +1,13 @@
+print('hello, world!')
+print("你好,世界！")
+print('你好', '世界')
+print('hello', 'world', sep=', ', end='!')
+print('goodbye, world', end='!\n')
+
+'''
+docker exec python380_c python3 python100days/day01/hello.py
+'''
+
+"""
+1. https://github.com/jackfrued/Python-100-Days/blob/master/Day01-15/code/Day01/hello.py
+"""
