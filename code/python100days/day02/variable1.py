c4c248412f6546f98488dd216d5f5d9ef69af1a2
zzz <xaozhuge@163.com>
2024-01-08 22:19:53 +0800

    variable1.py

---
+++ code/python100days/day02/variable1.py
@@ -0,0 +1,25 @@
+
+
+a = 321
+b = 123
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+print(3 ** 4)
+
+'''
+docker exec python380_c python3 python100days/day02/variable1.py
+'''
+
+"""
+使用变量保存数据并进行操作
+1. a/b 表示 a除以b的结果，得到的是一个浮点数
+2. a //b 将变量a除以b的结果只保留整数部分，舍弃小数部分
+3. a % b 返回变量a除以b的余数
+4. a ** b 返回变量a的变量b幂次方。 
+
+"""
