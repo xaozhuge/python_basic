46b9f59a4d5394b3cec197e54f9999ce105d0f44
zzz <xaozhuge@163.com>
2024-01-09 20:08:10 +0800

    variable4.py

---
+++ code/python100days/day02/variable4.py
@@ -0,0 +1,23 @@
+
+a = 100
+b = 1000000000000000000
+c = 12.345
+d = 1 + 5j
+e = 'A'
+f = 'hello, world'
+g = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+
+'''
+docker exec -it python380_c python3 python100days/day02/variable4.py
+'''
+
+"""
+1. 检查变量的类型
+"""
