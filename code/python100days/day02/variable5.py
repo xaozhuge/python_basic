56536c35014ba1a619ca2e9785a5d0bb71808dad
zzz <xaozhuge@163.com>
2024-01-10 10:09:17 +0800

    variable5.py

---
+++ code/python100days/day02/variable5.py
@@ -0,0 +1,52 @@
+
+a = 100
+b = str(a)
+c = 12.345
+d = str(c)
+e = '123'
+f = int(e)
+g = '123.456'
+h = float(g)
+i = False
+j = str(i)
+k = 'hello'
+m = bool(k)
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+print(c)
+print(type(c))
+print(d)
+print(type(d))
+print(e)
+print(type(e))
+print(f)
+print(type(f))
+print(g)
+print(type(g))
+print(h)
+print(type(h))
+print(i)
+print(type(i))
+print(j)
+print(type(j))
+print(k)
+print(type(k))
+print(m)
+print(type(m))
+
+'''
+docker exec -it python380_c python3 python100days/day02/variable5.py
+'''
+
+"""
+1. 类型转换
+数字转字符串
+浮点数转字符串
+字符串转数字
+字符串转浮点数
+布尔值转字符串
+字符串转布尔值
+
+"""
