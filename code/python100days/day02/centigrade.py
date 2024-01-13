99ae8243f93292f03482d94a2b7d9ccfef75a54f
zzz <xaozhuge@163.com>
2024-01-13 22:56:38 +0800

    centigrade.py

---
+++ code/python100days/day02/centigrade.py
@@ -0,0 +1,14 @@
+
+f = float(input('请输入华氏温度: '))
+c = (f - 32) / 1.8
+print('%.1f华氏度 = %.1f摄氏度' % (f, c))
+
+'''
+docker exec -it python380_c python3 python100days/day02/centigrade.py
+'''
+
+"""
+1. 将华氏温度转换为摄氏温度
+F = 1.8C + 32
+
+"""
